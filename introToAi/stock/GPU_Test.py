70812892e8ee8df25502ed2b9db6b4acc7af80ca
Jzzzi <liujk22@mails.tsinghua.edu.cn>
2024-04-18 20:20:49 +0800

    stock

--- introToAi/stock/GPU_Test.py
+++ introToAi/stock/GPU_Test.py
@@ -1,8 +1,10 @@
 import torch
 
 # 打印所有可用GPU
-for i in range(torch.cuda.device_count()):
-    print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
-
-# 设置当前设备为GPU0
-torch.cuda.set_device(0)
+if torch.cuda.device_count() == 0:
+    print("No GPU available.")
+else:
+    for i in range(torch.cuda.device_count()):
+        print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
+        # 设置当前设备为GPU0
+        torch.cuda.set_device(0)
