4008c15cd084ade9bfabb08fc488e28eddc54d16
Jzzzi <liujk22@mails.tsinghua.edu.cn>
2024-04-29 23:37:54 +0800

    clash

--- conf/clash/launch.sh
+++ conf/clash/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gnome-terminal --tab --title=clash -- bash -c "gsettings set org.gnome.system.proxy mode 'manual' && cd /home/jzi/clash && ./clash -d .;exec bash" 
+gnome-terminal --tab --title=clash -- bash -c "gsettings set org.gnome.system.proxy mode 'manual' && cd /home/jzi/Courses/conf/clash && ./clash -d .;exec bash" 
 gnome-terminal --tab --title=edge -- bash -c "microsoft-edge https://clash.razord.top/"
-gnome-terminal --tab --title=proxyclose -- bash -c "/home/jzi/clash/proxyclose && gsettings set org.gnome.system.proxy mode 'none' && exit;exec bash"
+gnome-terminal --tab --title=proxyclose -- bash -c "/home/jzi/Courses/conf/clash/proxyclose && gsettings set org.gnome.system.proxy mode 'none' && exit;exec bash"
